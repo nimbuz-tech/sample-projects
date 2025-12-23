e71c70fdf7cadf91603acd8fe0e1be695acc4b67
Rajeshwaran Murugesan <rajeshwaran.m@payorch.com>
2025-12-23 17:04:09 +0530

    removed java fikes to reupload

--- java/v17/maven-3.9.9-mongo/src/main/java/com/example/Course/Registration/System/repository/CourseRegistryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.Course.Registration.System.repository;
-
-import com.example.Course.Registration.System.model.CourseRegistry;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRegistryRepo extends MongoRepository<CourseRegistry, String> {
-}
