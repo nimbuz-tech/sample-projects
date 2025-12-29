45895f0f51eb4170047a05e739f803eace7320cf
Rajeshwaran Murugesan <rajeshwaran.m@payorch.com>
2025-12-29 17:45:06 +0530

    Removed the angluar files commited

--- v16/inventory-app/src/assets/app/features/orders/orders.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { OrderListComponent } from './pages/order-list/order-list.component';
-import { OrderCreateComponent } from './pages/order-create/order-create.component';
-
-export const ORDERS_ROUTES: Routes = [
-  { path: '', component: OrderListComponent },
-  { path: 'new', component: OrderCreateComponent },
-];
--- v9/inventory-app/src/assets/app/features/products/products.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProductListComponent } from './pages/product-list/product-list.component';
-import { ProductFormComponent } from './pages/product-form/product-form.component';
-
-export const PRODUCTS_ROUTES: Routes = [
-  { path: '', component: ProductListComponent },
-  { path: 'new', component: ProductFormComponent },
-  { path: ':id', component: ProductFormComponent },
-];
